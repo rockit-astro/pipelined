de355086123ae56f74bd8609f5c74d5711fb6e63
Paul Chote <p.chote@warwick.ac.uk>
2023-09-07 20:47:01 +0000

    Allow preview to show filter instead of camera.

--- rockit/pipeline/config.py
+++ rockit/pipeline/config.py
@@ -171,7 +171,7 @@ CAMERA_CONFIG_SCHEMA = {
         'input_data_path', 'output_data_path',
         'platescale', 'image_region_card', 'object_minpix',
         'preview_min_interval', 'preview_max_instances', 'preview_ds9_width', 'preview_ds9_height',
-        'preview_ds9_zoom', 'preview_ds9_annotation_margin',
+        'preview_ds9_zoom', 'preview_ds9_annotation_margin', 'preview_display_camera_id', 'preview_display_filter'
         # Note: overscan_region_card, binning_card are optional
     ],
     'properties': {
@@ -223,6 +223,12 @@ CAMERA_CONFIG_SCHEMA = {
         },
         'preview_ds9_annotation_margin': {
             'type': 'number'
+        },
+        'preview_display_camera_id': {
+            'type': 'boolean'
+        },
+        'preview_display_filter': {
+            'type': 'boolean'
         },
         'hfd_grid_tiles_x': {
             'type': 'integer',
